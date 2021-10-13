36dc38e5d66be4a29ebb26e7525f49c2788be373
chungnt <chungnt.mta@gmail.com>
2021-10-13 09:41:38 +0700

    init

---
+++ android/app/src/main/java/com/rnwithnative/MainNativeActivity.java
@@ -0,0 +1,16 @@
+package com.rnwithnative;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainNativeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_native);
+
+        
+    }
+}
