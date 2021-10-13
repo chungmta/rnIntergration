315ba8da1c73790cccf79e1d7ef0034e69ae6d0b
chungnt <chungnt.mta@gmail.com>
2021-10-13 10:56:22 +0700

    refactor

--- src/screen/FavoriteScreen/index.tsx
+++ src/screen/FavoriteScreen/index.tsx
@@ -2,14 +2,30 @@ import React from "react";
 import { View, Text } from "react-native";
 
 interface Props {
+  //receive from native
   message: string;
 }
 const FavoriteScreen = (props: Props) => {
   console.log("----", props);
 
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        borderWidth: 1,
+        borderColor: "red",
+        flexDirection: "column",
+        // justifyContent: "space-between",
+      }}>
       <Text>HELLO Native</Text>
+      <View
+        style={{
+          // alignSelf: "flex-end",
+          backgroundColor: "red",
+          height: 20,
+          width: "100%",
+        }}
+      />
     </View>
   );
 };
